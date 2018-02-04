a66c3bfbc4ad575d63e0ed428127ee7394c6d2ec
Unknown <tntdragonmc@gmail.com>
2018-02-04 13:26:24 +0100

    Dinal check

--- code/go.js
+++ code/go.js
@@ -145,7 +145,7 @@ bot.on("message", function (message) {
             "and if you think you have an idea what the secret word is, do \"-guess WORD\"");
     }
     //#discord_suggestions auto-reaction system
-    if(message.channel.id == "409681451341053952" && (message.author.roles.find("name","Discord Helper") == undefined)){
+    if(message.channel.id == "409681451341053952" && (message.author.roles.find("name","Discord Helper"))){
         message.react(message.guild.emojis.get("303100818041733120")).catch(function () {
             logging.legacyLog("Fatal Error in adding agree rating.");
         });
