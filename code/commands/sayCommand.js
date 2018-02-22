6bd2e35d48143f05b1890d5a94c2a5e33d63a0f0
Unknown <tntdragonmc@gmail.com>
2018-02-22 14:48:59 +0100

    Full automatization test
    Hopefully it works

--- code/commands/sayCommand.js
+++ code/commands/sayCommand.js
@@ -2,14 +2,14 @@ module.exports = {
     description: "Command used to say anything in (almost) any channel",
     usage: "-say {Chat} {Message}",
     allowedInDM: false,
-    allowedChannels: ["274488503243636737"],
+    allowedChannels: [config.settings.logChannelID],
     call: function(message, args){
-        if (bot.guilds.get("262699181620068352").channels.find("name", args[0]) != null && config.settings.adminChannels.includes(bot.guilds.get("262699181620068352").channels.find("name", args[0]).id)) {
+        if (bot.guilds.get(config.settings.serverID).channels.find("name", args[0]) != null && config.settings.adminChannels.includes(bot.guilds.get(config.settings.serverID).channels.find("name", args[0]).id)) {
 			message.reply("I'm sorry, but I cannot send a message to that channel");
-        }else if (bot.guilds.get("262699181620068352").channels.find("name", args[0]) != null) {
+        }else if (bot.guilds.get(config.settings.serverID).channels.find("name", args[0]) != null) {
 			var channelName = args[0];
 			args[0] = " ";
-			bot.guilds.get("262699181620068352").channels.find("name", channelName).sendMessage(args.join(' '))
+			bot.guilds.get(config.settings.serverID).channels.find("name", channelName).sendMessage(args.join(' '))
 			.catch(function () {
               console.log("Error in posting to the #" + channelName + " channel.");
             });
