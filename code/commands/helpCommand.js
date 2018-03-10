12987159445b0c7087239ac290450a9c210dcdcc
Unknown <tntdragonmc@gmail.com>
2018-03-10 20:54:00 +0100

    More udpates

--- code/commands/helpCommand.js
+++ code/commands/helpCommand.js
@@ -2,7 +2,7 @@ module.exports = {
     description: "Provides help for all public BeeBot 2.0 commands",
     usage: "-help command",
     allowedInDM: true,
-    allowedChannels: [],
+    allowedChannels: ["Cookies","and","spice","and","everything","nice"],
     call: function(message, args){
         //turning all available commands into an object
         const command = require("./commands.js");
@@ -10,7 +10,8 @@ module.exports = {
             var description = command[c].description;
             var usage = command[c].usage;
             var DMAllowance = command[c].allowedInDM;
-            return [c, description, usage, DMAllowance];
+            var otherChannels = command[c].allowedChannels;
+            return [c, description, usage, DMAllowance, otherChannels];
         });
         message.reply(commands);
     }
