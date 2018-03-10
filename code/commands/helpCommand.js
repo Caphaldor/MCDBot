189428e581b4b317a86fc96ca7e55db3004f7692
Unknown <tntdragonmc@gmail.com>
2018-03-10 20:46:41 +0100

    More updates

--- code/commands/helpCommand.js
+++ code/commands/helpCommand.js
@@ -6,7 +6,12 @@ module.exports = {
     call: function(message, args){
         //turning all available commands into an object
         const command = require("./commands.js");
-        var commands = Object.keys(command);
+        var commands = Object.keys(command).map(function(c) {
+            var description = command[c].description;
+            var usage = command[c].usage;
+            var DMAllowance = command[c].allowedInDM;
+            return [c, description, usage, DMAllowance];
+        });
         message.reply(commands);
     }
 };
