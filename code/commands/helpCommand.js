b1643fce25153e66c106856ba85b02b9886f75b5
Unknown <tntdragonmc@gmail.com>
2018-03-10 21:23:09 +0100

    forgot to replace 1's with 0's

--- code/commands/helpCommand.js
+++ code/commands/helpCommand.js
@@ -14,8 +14,8 @@ module.exports = {
             channels[c] = command[c].allowedChannels;
             return [c, description, usage, DMAllowance, "\n"];
         }).sort(function(a,b){
-            if(a[1] < b[1]) return -1;
-            if(a[1] > b[1]) return 1;
+            if(a[0] < b[0]) return -1;
+            if(a[0] > b[0]) return 1;
             return 0;
         });
         message.reply(commands + "\n\n" + channels.help);
