5a9911ae2d3f060155c230d2c14bf859b3716c1b
Unknown <tntdragonmc@gmail.com>
2018-03-11 12:34:01 +0100

    Ma bad

--- code/commands/helpCommand.js
+++ code/commands/helpCommand.js
@@ -22,7 +22,7 @@ module.exports = {
         //commands[c] requires number not name
             var availableCommands = Object.keys(command).map(function(c) {
                 for (i=0; i<commands.length;i++) {
-                    if (commands[i][0]==c && commands[i][3]) {return[c, commands.c[1], commands.c[2]];message.reply("Beep\n");}
+                    if (commands[i][0]==c && commands[i][3]) {message.reply("Beep\n");return[c, commands[i][1], commands[i][2]];message.reply("Beep\n");}
                 }
             });
         }
