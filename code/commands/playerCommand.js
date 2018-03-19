d9c235c73a0e9537ab7cefed6212ca1bde2578f2
Unknown <tntdragonmc@gmail.com>
2018-03-19 10:58:31 +0100

    Disabling -player for a moment

--- code/commands/playerCommand.js
+++ code/commands/playerCommand.js
@@ -46,7 +46,7 @@ module.exports = {
             req("http://api.hivemc.com/v1/player/" + args[0], function (error, response, body) {
                 if (error){logging.legacyLog("URGENT HTTP ERROR")}
                 message.reply(body);
-                if (body != undefined){
+                if (false){
                     var hiveData = JSON.parse(body);
                     var color = "green";
 					var onlineCheck = "";
