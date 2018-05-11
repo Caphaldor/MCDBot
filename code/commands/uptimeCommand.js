5f19da11444af9c4586f2817726a79fe0b6bb081
Unknown <tntdragonmc@gmail.com>
2018-05-11 10:59:10 +0200

    I should use fields, but too lazy

--- code/commands/uptimeCommand.js
+++ code/commands/uptimeCommand.js
@@ -4,12 +4,12 @@ module.exports = {
     allowedInDM: false,
     allowedChannels: ["274488503243636737"],
     call: function(message, args){
-        logging.log("Bot information", "BeeBot 2.0 has been up for " +
+        logging.log("Bot information", "**BeeBot 2.0 has been up for:** " +
             "\n" +
             Math.floor(bot.uptime / 86400000) + " days\n" +
             Math.floor(bot.uptime / 3600000 % 24) + " hours\n" +
             Math.floor(bot.uptime / 60000 % 60) + " minutes and\n" +
-            Math.floor(bot.uptime / 1000 % 60) + " seconds.\n" +
+            Math.floor(bot.uptime / 1000 % 60) + " seconds.\n\n" +
             "**Current Bot Version:** " + botInfo.version + "\n" +
             "**Bot License:** " + botInfo.license, "blue");
     }
