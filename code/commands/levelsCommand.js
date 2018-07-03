8b173329705d8063501ec0d819dc4bd19cbcbb0d
Unknown <tntdragonmc@gmail.com>
2018-07-03 18:30:03 +0200

    Fufth time's the game-changer?

--- code/commands/levelsCommand.js
+++ code/commands/levelsCommand.js
@@ -101,7 +101,7 @@ module.exports = {
                         var fieldNum=0;
                         var fieldInformation = [{title:"Unplayed blocks",count:0},{title:"Levels 1-4",count:0},{title:"Levels 5-9",count:0},{title:"Levels 10-14",count:0},{title:"Levels 15-19",count:0},{title:"Levels 20-24",count:0},{title:"Levels 25-29",count:0},{title:"Levels 30-34",count:0},{title:"Levels 35-39",count:0},{title:"Levels 40-44",count:0},{title:"Levels 45-49",count:0},{title:"Max level blocks",count:0}];
                         for (i=0; expInfo[i] != undefined; i++) {
-                            fieldInformation[Math.ceil((expInfo[2]/5)+0.1)].count++;
+                            fieldInformation[Math.ceil((expInfo[i][2]/5)+0.1)].count++;
                         }
                         var fieldCounter = 0;
                         for (i=0; i<12;i++) {
