1f0e7fe23758f940a5122eef940a407da6011f55
Unknown <tntdragonmc@gmail.com>
2018-07-03 19:05:15 +0200

    Eight time's the finisher.

--- code/commands/levelsCommand.js
+++ code/commands/levelsCommand.js
@@ -99,7 +99,7 @@ module.exports = {
                         });
                         var messageFields = [];
                         var fieldNum=0;
-                        var fieldInformation = [{title:"Unplayed blocks",count:0},{title:"Levels 1-4",count:0},{title:"Levels 5-9",count:0},{title:"Levels 10-14",count:0},{title:"Levels 15-19",count:0},{title:"Levels 20-24",count:0},{title:"Levels 25-29",count:0},{title:"Levels 30-34",count:0},{title:"Levels 35-39",count:0},{title:"Levels 40-44",count:0},{title:"Levels 45-49",count:0},{title:"Max level blocks",count:0}];
+                        var fieldInformation = [{title:"Unplayed blocks",count:0},{title:"Levels 1-4",count:0},{title:"Levels 5-9",count:0},{title:"Levels 10-14",count:0},{title:"Levels 15-19",count:0},{title:"Levels 20-24",count:0},{title:"Levels 25-29",count:0},{title:"Max Level Blocks",count:0},{title:"Levels 35-39",count:0},{title:"Levels 40-44",count:0},{title:"Levels 45-49",count:0},{title:"Max Level Blocks",count:0}];
                         for (i=0; expInfo[i] != undefined; i++) {
                             fieldInformation[Math.ceil((expInfo[i][2]/5)+0.1)].count++;
                         }
@@ -121,6 +121,7 @@ module.exports = {
                         message.reply("",
                             {
                                 embed: {
+                                "title": "Overview of Hide and Seek levels for " + args[0],
                                 "color": 0xFFD700,
                                 "fields": messageFields
                                 }
