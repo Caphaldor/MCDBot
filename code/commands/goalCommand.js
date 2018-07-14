06e93ea94e47a0a4b3d5097ffc360564637ce7e8
Unknown <tntdragonmc@gmail.com>
2018-07-15 01:30:39 +0200

    I should go to sleep

--- code/commands/goalCommand.js
+++ code/commands/goalCommand.js
@@ -93,7 +93,7 @@ module.exports = {
                     //(takes goal/game from current stats)
                     var timeNeeded = ((needed*hiveData[goalsConfig[args[1].toLowerCase()].games])/actualAmount)*(goalsConfig[args[1].toLowerCase()].gameTime.lobbyTime + (goalsConfig[args[1].toLowerCase()].gameTime.lowAwerage + goalsConfig[args[1].toLowerCase()].gameTime.highAverage)/2);
                     var timeToGoal = timeEstimator(timeNeeded/60);
-                    message.reply(timeNeeded + "\n" + goalsConfig[args[1].toLowerCase()].gameTime.lobbyTime);
+                    message.reply(needed + "\n" + hiveData[goalsConfig[args[1].toLowerCase()].games] + "\n" + actualAmount);
                 break;
                 //Ratio goals
                 case "ratio":
