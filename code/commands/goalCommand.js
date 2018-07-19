71fa041d7e9fc39e6cc63e5e1a4d684940bc5e08
Unknown <tntdragonmc@gmail.com>
2018-07-19 22:44:20 +0200

    Uhm....

--- code/commands/goalCommand.js
+++ code/commands/goalCommand.js
@@ -166,7 +166,7 @@ module.exports = {
                     if (rankPos == (gameTitles.length-1)|| rankPos == 0) {goalReached = true; break;}
                     //Check next rank requirements
                     var nextRank = gameTitles[rankPos+1].plain_name;
-                    var needed = gameTitles[rankPos+1].required_points - currentPoints;
+                    var needed = gameTitles[parseInt(rankPos)+1].required_points - currentPoints;
                     //calculate the time needed to get to it
                     var timeNeeded = ((needed*games)/currentPoints)*averageGameTime;
                     var timeToGoal = timeEstimator(timeNeeded/60);
