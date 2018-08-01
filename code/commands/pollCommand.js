ac9ff2c200f1a66d92bf9769321b442b1076aa36
Unknown <tntdragonmc@gmail.com>
2018-08-01 17:48:02 +0200

    Better fix?

--- code/commands/pollCommand.js
+++ code/commands/pollCommand.js
@@ -4,13 +4,13 @@ module.exports = {
     allowedInDM: false,
     allowedChannels: ["274488503243636737"],
     call: function(message, args){
-        if (args.includes("|")) {
+        if (!args.includes("|")) {
             bot.channels.get(config.settings.pollChannelID).send(
                 "Detected a yes or no question"
             ).then(msg =>
                 msg.react(message.guild.emojis.get("303100818041733120")).catch(function () {
                     logging.legacyLog("Fatal Error in adding agree rating.");
-                })).then(msg =>
+                })).then(
                setTimeout(function() {msg.react(message.guild.emojis.get("303100826434404362")).catch(function () {
                     logging.legacyLog("Fatal Error in adding disagree rating.");
                 })}, 1000)
