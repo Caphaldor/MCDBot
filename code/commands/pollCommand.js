457abd61422ed913acf1d3fdabd70a8cfd5c9fcb
Unknown <tntdragonmc@gmail.com>
2018-08-01 20:46:07 +0200

    Testing emoji stuff

--- code/commands/pollCommand.js
+++ code/commands/pollCommand.js
@@ -7,9 +7,9 @@ function addSimpleReactions(message) {
     })}, 1000);
     //timeout exists so that agree is always before disagree
 }
-function addComplexReactions(message, options) {
-    message.react(message.guild.emojis.get("303100818041733120")).catch(function () {
-        logging.legacyLog("Fatal Error in adding agree rating.");
+function addComplexReactions(message, amount) {
+    message.react(message.guild.emojis.get("474283452934651914")).catch(function () {
+        logging.legacyLog("Fatal Error in adding A rating.");
     });
 }
 function findSeparators(inputArray) {
@@ -33,10 +33,21 @@ module.exports = {
                 "",{embed: embed("Poll from " + message.author.username,args.join(" "), "gold")}
             ).then(msg => addSimpleReactions(msg));
         } else {
-            var separators = findSeparators(args);
+            var separators = findSeparators(args),response="";
+            separators.push(args.length);
+            for (i=0;i<separators[0];i++) {
+                response += args[i] + " ";
+            }
+            for (i=0;i<(separators.length-1);i++) {
+                var temp = "";
+                for (j=(separators[i]+1);j<separators[i+1];j++) {
+                    temp += args[j] + " ";
+                }
+                response += "\n" + settings.emojiCodes[i] + " - " + temp;
+            }
             bot.channels.get(config.settings.pollChannelID).send(
-                "",{embed: embed("Poll from " + message.author.username,"found " + separators.length + " separators, first after " + args[separators[0]] , "gold")}
-            ).then(msg => addComplexReactions(msg));
+                "",{embed: embed("Poll from " + message.author.username,"found " + separators.length + " separators, first after " + args[separators[0]-1] + response , "gold")}
+            ).then(msg => addComplexReactions(msg,(separators.length-2)));
         }
     }
 };
