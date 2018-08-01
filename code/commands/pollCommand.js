2f068423de7ef188778d86f3c1fa3811f5063755
Unknown <tntdragonmc@gmail.com>
2018-08-01 17:36:58 +0200

    More tests

--- code/commands/pollCommand.js
+++ code/commands/pollCommand.js
@@ -4,6 +4,21 @@ module.exports = {
     allowedInDM: false,
     allowedChannels: ["274488503243636737"],
     call: function(message, args){
-        bot.channels.get("274488503243636737").send("Just a poll test");
+        if (args.includes("|")) {
+            bot.channels.get(config.settings.pollChannelID).send(
+                "Detected a yes or no question"
+            ).then(msg =>
+                msg.react(message.guild.emojis.get("303100818041733120")).catch(function () {
+                    logging.legacyLog("Fatal Error in adding agree rating.");
+                });
+               setTimeout(function() {message.react(message.guild.emojis.get("303100826434404362")).catch(function () {
+                    logging.legacyLog("Fatal Error in adding disagree rating.");
+                })}, 1000);
+            );
+        } else {
+            bot.channels.get(config.settings.pollChannelID).send(
+                "",{embed: embed("` `","Just testing embeds", "gold")}
+            );
+        }
     }
 };
