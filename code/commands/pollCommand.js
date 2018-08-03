e421c22898308b2bea92f728dc6ef1b4662458c0
Unknown <tntdragonmc@gmail.com>
2018-08-03 13:59:08 +0200

    Added -poll help

--- code/commands/pollCommand.js
+++ code/commands/pollCommand.js
@@ -28,6 +28,12 @@ function findSeparators(inputArray) {
     }
     return separatorPositions
 }
+//checks for deletion in case not in DM
+function checkDM(msg, DM, div) {
+    if (DM != "dm") {
+        msg.delete(30000/div);
+    }
+};
 module.exports = {
     description: "The command will create a poll in #polls",
     usage: "-poll {PolL Message} [\"| {Poll answer 1} | { Poll Answer 2} | [etc.]\"]",
@@ -37,7 +43,9 @@ module.exports = {
         if (!args.includes("|")) {
             var response = args.join(" ");
             if (!(response.replace(/\s+/g,""))) {
-                message.reply("",{embed: embed("Error","Is it just me, or did you forget to put a question in your poll?", "red")}).then(msg => checkDM(msg, message.channel.type, divN));;
+                message.reply("",{embed: embed("Error","Is it just me, or did you forget to put a question in your poll?", "red")}).then(msg => checkDM(msg, message.channel.type, 1));;
+            } else if (response == "help"){
+                 message.reply("",{embed: embed("`-poll` Help","Doing `-poll {Question}` will create a simple \"Yes\" or \"No\" poll. You can create custom answers for your poll by adding `| Answer A | Answer B | Answer C` etc. at the end. For example:\nDoing `-poll Is this guide helpful?` Will create the poll and automatically add <:303100818041733120:> and <:303100826434404362> to it.\nDoing `-poll Is this guide helpful? | Yes | Maybe | No` will create the very same poll, but will associate each of the provided answers with 🇦, 🇧 and 🇨 respectively.\n\n*Do note that you need a space before and after the `|`, as well as actual answers and not empty space in-between them, otherwise it will be seen as a character in your question/answer.*", "white")}).then(msg => checkDM(msg, message.channel.type, 1));;
             } else {
             bot.channels.get(config.settings.pollChannelID).send(
                 "",{embed: embed("Poll from " + message.author.username,response, "pink")}
@@ -46,14 +54,14 @@ module.exports = {
         } else {
             var separators = findSeparators(args),response="";
             if (separators.length<2 || separators.length>20) {
-                message.reply("",{embed: embed("Error","You have provided an invalid number of options.\nI need at least 2 and no more than 20.", "red")}).then(msg => checkDM(msg, message.channel.type, divN));;
+                message.reply("",{embed: embed("Error","You have provided an invalid number of options.\nI need at least 2 and no more than 20.", "red")}).then(msg => checkDM(msg, message.channel.type, 1));;
             } else {
             separators.push(args.length);
             for (i=0;i<separators[0];i++) {
                 response += args[i] + " ";
             }
             if (!(response.replace(/\s+/g,""))) {
-                message.reply("",{embed: embed("Error","Is it just me, or did you forget to put a question in your poll?", "red")}).then(msg => checkDM(msg, message.channel.type, divN));;
+                message.reply("",{embed: embed("Error","Is it just me, or did you forget to put a question in your poll?", "red")}).then(msg => checkDM(msg, message.channel.type, 1));;
             } else {
             for (i=0;i<(separators.length-1);i++) {
                 var temp = "";
