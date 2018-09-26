cff1791ec27d374ed07d47bb84b6f16ed459babf
Unknown <tntdragonmc@gmail.com>
2018-09-26 20:06:11 +0200

    done

--- code/commands/goalCommand.js
+++ code/commands/goalCommand.js
@@ -61,7 +61,7 @@ module.exports = {
             message.reply("",{embed: embed("`-goal` Help",response,"white")}).then(msg => checkDM(msg, message.channel.type, divN));
         } else {
         if (goalsConfig[args[1].toLowerCase()] == undefined) {
-            message.reply("",{embed: embed("Error","I'm sorry, but we do not support this gamemode yet. Make sure that `" + args[1] + "` is the proper game code for what you're looking for.\n*(full list of codes at `-stats list`)", "red")}).then(msg => checkDM(msg, message.channel.type, divN));
+            message.reply("",{embed: embed("Error","I'm sorry, but we do not support this gamemode yet. Make sure that `" + args[1] + "` is the proper game code for what you're looking for.\n*(full list of codes at `-stats list`)*", "red")}).then(msg => checkDM(msg, message.channel.type, divN));
         } else {
         if (!goalsConfig[args[1].toLowerCase()].availableGoals.includes(args[2].toLowerCase())) {
             message.reply("",{embed: embed("Error","I'm sorry, but I cannot check for this goal yet. Do `-goal " + args[1] + " available` to get the available goals.", "red")}).then(msg => checkDM(msg, message.channel.type, divN));
